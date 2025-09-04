e98268bc0b3cc072b7b77fe025ac4c4007708347
Tartertx <tartertx@dukes.jmu.edu>
2025-09-04 12:00:07 -0400

    Update pascal.m

--- pascal.m
+++ pascal.m
@@ -10,10 +10,10 @@
 
 %% Code
 
-n = 5;
+n = 10;
 A = zeros(n);
-for i = 0:n
-    for j = 0:n
+for i = 0:n-1
+    for j = 0:n-1
         if j <= i
             A(i+1,j+1) = factorial(i)/(factorial(i-j)*factorial(j));
         end
