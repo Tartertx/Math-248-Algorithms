1a1304a9f04a5ee1e47f0b223358e6878694ed2e
Tartertx <tartertx@dukes.jmu.edu>
2025-09-04 11:58:17 -0400

    pascal

---
+++ pascal.m
@@ -0,0 +1,23 @@
+%% Preamble
+% Program name:     pascal.m
+% Author:           Timothy Tarter
+% Due Date:         09/04/2025
+% Purpose:          Build out Pascal's Triangle.
+
+%% Variables
+% n is the size of the nxn matrix
+% A is the matrix
+
+%% Code
+
+n = 5;
+A = zeros(n);
+for i = 0:n
+    for j = 0:n
+        if j <= i
+            A(i+1,j+1) = factorial(i)/(factorial(i-j)*factorial(j));
+        end
+    end
+end
+
+disp(A)
