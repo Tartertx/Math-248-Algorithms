8f06bda1dfd206ae3417f85470a005a228e7beb2
Tartertx <tartertx@dukes.jmu.edu>
2025-10-06 13:32:00 -0400

    updates

--- fpi.m
+++ fpi.m
@@ -9,26 +9,50 @@
 % x is the row vector of FPI entries
 % tol is the error tolerance
 % n is the maximum number of trials we are willing to have
-% fixedpoint is the estiamted fixed points
+% fixedpoint is the table output
 
 %% Code
 function fixedpoint = fpi(x0, tol, n)
-    function y=g(x)
-        y = 2.^(-x);
+    %deal with the number of inputs
+    if nargin == 1
+        tol = 0.001;
+        n = 1000;
     end
+    if nargin == 2
+        n = 1000;
+    end
+
+    %set up initial conditions for the table
     format longG
     x = zeros(n,2);
     x(1,1)=x0;
     x(1,2)=0;
+
+    %run the FPI algorithm
     for i = 1:n
-        x(i+1,1) = g(x(i,1));
+        x(i+1,1) = g(x(i,1)); %update the x vector with g(x) and i
         x(i+1,2)=i;
-        if abs(x(i,1)-x(i+1,1)) <= tol*x(i+1,1)
-            x=x(1:i,:);
+        if abs(x(i,1)-x(i+1,1)) <= tol*x(i+1,1) %if the difference between terms is within the error bound, end the loop
+            x=x(1:i,:); %chop off the last terms which are zeroed
             break
         end
     end
-    fpidata = x(:,1);
-    iteration = x(:,2);
-    fixedpoint = table(iteration,fpidata);
+    
+    %if we didn't end early, output an error
+    if i == n 
+            failureOutput = ['The method failed after ', num2str(n),' iterations.'];
+            error(failureOutput);
+    end
+    
+    %if we ended early, output the table
+    if i < n
+        fpidata = x(:,1);
+        iteration = x(:,2);
+        fixedpoint = table(iteration,fpidata);
+    end
+
+    %subfunction for computing g(x)
+    function y=g(x)
+        y = 2.^(-x);
+    end
 end
